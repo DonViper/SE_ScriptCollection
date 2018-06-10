3d7b61eb47ef3d091f5a3ea20436eed5c9d8b9aa
Mike Mortensen <ZhakalenDk@users.noreply.github.com>
2018-06-10 23:47:04 +0200

    Script working now!

--- Script_Collection/SE_ScriptCollection/Stations/GlacialisTechFacility.cs
+++ Script_Collection/SE_ScriptCollection/Stations/GlacialisTechFacility.cs
@@ -22,12 +22,10 @@ namespace GlacialisTechFacility
         //To put your code in a PB copy from this comment...
 
         #region CONSTANTS
-        const string LCD_BLOCK_GROUP_NAME = "LCD's (Glacialis Tech Facility)";
-        const string SINGLE_LCD_NAME = "LCD (Glacialis Tech Facilty)";
+        const string LCD_BLOCK_GROUP_NAME = "LCD's";
+        const string SINGLE_LCD_NAME = "LCD";
         readonly string [] LCD_TAGS = { "<Power>", "<Docking>", "<Storage>", "<Inventory>" };
         #endregion
-
-        int G_isRunning = 0;
         bool G_useBlockGroup = true;
 
         List<IMyTextPanel> G_screens;
@@ -45,21 +43,25 @@ namespace GlacialisTechFacility
 
         public void Main ( string argument, UpdateType updateType )
         {
-            Echo ( Run_Indicator () );  //  Visual representation of the scrip running
 
             G_useBlockGroup = Fecth_LCD_Block_Group ();
 
             if ( G_useBlockGroup == true )
             {
-                Docking ();
+                for ( int i = 0; i < G_screens.Count; i++ )
+                {
+                    G_screens [i].WritePublicText ( "<-Terminal->\n", false );
+                    G_screens [i].WritePublicText ( "<--------->", true );
+                }
             }
             else //  If there is no block group
             {
                 LCD = GridTerminalSystem.GetBlockWithName ( SINGLE_LCD_NAME ) as IMyTextPanel;
                 if ( LCD != null )
                 {
-                    LCD.WritePublicText ( "<-Terminal->", false );
-                    Docking ();
+                    Echo ( $"Single LCD mode" );
+                    LCD.ShowPublicTextOnScreen ();
+                    LCD.WritePublicText ( "<-Terminal->\n", false );
                     LCD.WritePublicText ( "<--------->", true );
                 }
                 else
@@ -77,10 +79,14 @@ namespace GlacialisTechFacility
         /// <returns></returns>
         private bool Fecth_LCD_Block_Group ()
         {
-            List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock> ();
-            GridTerminalSystem.GetBlockGroupWithName ( LCD_BLOCK_GROUP_NAME ).GetBlocks ( blocks );
 
-            if ( blocks.Count < 0 )
+            List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock> ();
+            if ( GridTerminalSystem.GetBlockGroupWithName ( LCD_BLOCK_GROUP_NAME ) != null )
+            {
+                GridTerminalSystem.GetBlockGroupWithName ( LCD_BLOCK_GROUP_NAME ).GetBlocks ( blocks );
+            }
+
+            if ( blocks.Count > 0 )
             {
                 G_screens = new List<IMyTextPanel> ();
 
@@ -96,85 +102,13 @@ namespace GlacialisTechFacility
                     G_screens.Add ( LCD );
                 }
 
+                Echo ( $"Using LCD group." );
                 return true;
             }
 
-            Echo ( $"No LCD block group with the name ({LCD_BLOCK_GROUP_NAME}) found. Switching to single LCD use." );
+            Echo ( $"LCD group ({LCD_BLOCK_GROUP_NAME}) missing." );
 
             return false;
-        }
-
-        /// <summary>
-        /// Info on docked ships
-        /// </summary>
-        private void Docking ()
-        {
-            if ( G_useBlockGroup == true )  //  If there is a block group
-            {
-                for ( int i = 0; i < G_screens.Count; i++ )
-                {
-                    if ( G_screens [i].CustomName.Contains ( LCD_TAGS [1] ) )   //  If there is a docking tag
-                    {
-                        G_screens [i].WritePublicText ( "<----Docking---->", false );
-                        LCD.WritePublicText ( "No advanced info available", true );
-                        G_screens [i].WritePublicText ( "<--------------->", true );
-                    }
-                    else    //  If there is no docking tag
-                    {
-                        Show_Simple_Docking_Info ();
-                    }
-                }
-            }
-            else if ( LCD != null ) //  If there is no block group
-            {
-                Show_Simple_Docking_Info ();
-            }
-
-        }
-
-        /// <summary>
-        /// Simple version of docking info
-        /// </summary>
-        private void Show_Simple_Docking_Info ()
-        {
-            LCD.WritePublicText ( "<----Docking---->", true );
-            LCD.WritePublicText ( "No info available", true );
-            LCD.WritePublicText ( "<--------------->", true );
-        }
-
-        /// <summary>
-        /// Animation for indication of the script running
-        /// </summary>
-        /// <returns></returns>
-        private string Run_Indicator ()
-        {
-            string runText = "";
-            if ( G_isRunning == 0 )
-            {
-                runText = "    Running";
-            }
-            else if ( G_isRunning == 1 )
-            {
-                runText = "   =Running=";
-            }
-            else if ( G_isRunning == 2 )
-            {
-                runText = "  ==Running==";
-            }
-            else if ( G_isRunning == 3 )
-            {
-                runText = " ===Running===";
-            }
-            else if ( G_isRunning == 4 )
-            {
-                runText = "|===Running===|";
-            }
-            else
-            {
-                return null;
-            }
-
-            return runText;
         }
         //to this comment.
         #region post-script
