cf319f94662c83c5861bbfb67ea7b10863449a72
Mike Mortensen <ZhakalenDk@users.noreply.github.com>
2018-06-12 12:54:20 +0200

    Added tag support for Facility script

--- Script_Collection/SE_ScriptCollection/Stations/GlacialisTechFacility.cs
+++ Script_Collection/SE_ScriptCollection/Stations/GlacialisTechFacility.cs
@@ -63,6 +63,7 @@ namespace GlacialisTechFacility
             if ( G_useBlockGroup == false )
             {
                 G_LCD = GridTerminalSystem.GetBlockWithName ( SINGLE_LCD_NAME ) as IMyTextPanel;
+                //  If there is an LCD
                 if ( G_LCD != null )
                 {
                     G_LCD.ShowPublicTextOnScreen ();
@@ -73,6 +74,7 @@ namespace GlacialisTechFacility
                 }
 
             }
+            //  If there are no errors
             else if ( G_useBlockGroup == true && errorFLAG == false )
             {
                 debugText = "None";
@@ -83,34 +85,40 @@ namespace GlacialisTechFacility
         }
 
         /// <summary>
-        /// Fetch LCD block group if there is one
+        /// Fetch LCD block group and return true if there is one.
         /// </summary>
         /// <returns></returns>
         private bool Fecth_LCD_Block_Group ()
         {
 
             List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock> ();
+            //  Checking for block group
             if ( GridTerminalSystem.GetBlockGroupWithName ( LCD_BLOCK_GROUP_NAME ) != null )
             {
                 GridTerminalSystem.GetBlockGroupWithName ( LCD_BLOCK_GROUP_NAME ).GetBlocks ( blocks );
             }
 
+            //  If there is a block group and it's not empty
             if ( blocks.Count > 0 )
             {
                 G_screens = new List<IMyTextPanel> ();
 
+                //  Looping trough block group
                 for ( int element = 0; element < blocks.Count; element++ )
                 {
                     IMyTextPanel LCD = blocks [element] as IMyTextPanel;
-                    LCD.ShowPublicTextOnScreen ();
+                    LCD.ShowPublicTextOnScreen ();  //  Setting screens to display text
+
+                    //  If the screen still does not display text
                     if ( LCD.ShowText != true )
                     {
                         debugText = ( $"LCD with name ({LCD.CustomName}) could not be set to show public text on screen!\n" );
                     }
-                    LCD.FontColor = new Color ( 0, 255, 0 );
-                    G_screens.Add ( LCD );
+
+                    LCD.FontColor = new Color ( 0, 255, 0 );    //  CHange font color of the screen to green
+                    G_screens.Add ( LCD );  //  Add the screen to the G_Screens list
                 }
-                G_LCD = null;
+                G_LCD = null;   //  If there is a block group set single LCD to null
                 return true;
             }
 
@@ -119,28 +127,29 @@ namespace GlacialisTechFacility
             return false;
         }
 
+        /// <summary>
+        /// Fetch connectors on the grid
+        /// </summary>
         private void Fecth_Connectors ()
         {
             List<IMyShipConnector> tempList = new List<IMyShipConnector> ();
             GridTerminalSystem.GetBlocksOfType ( tempList );
 
-            //string names = "Connecters:\n";
-            //string name = "";
-
+            //  If there is any connectors on the base
             if ( tempList.Count > 0 )
             {
                 connectors = new List<IMyShipConnector> ();
-                //names = "";
+
+                //  Loop trough temporary list with connectors
                 for ( int i = 0; i < tempList.Count; i++ )
                 {
+                    //  If the connectors have the grid name in their name add it tot the connectors list.
                     if ( tempList [i].CustomName.Contains ( BASE_NAME ) )
                     {
                         connectors.Add ( tempList [i] );
-                        //name = tempList [i].CustomName;
-                        //names += $"{name}\n";
-
                     }
                 }
+                //  If no connectors with the grid name in their name was found.
                 if ( connectors.Count <= 0 )
                 {
                     debugText = $"No connectors with ({BASE_NAME}) in their name";
@@ -151,8 +160,9 @@ namespace GlacialisTechFacility
                     errorFLAG = false;
                 }
 
-                tempList = null;
+                tempList = null;    //  Set the temp list to null.
             }
+            //  If there is no connectors on the grid
             else
             {
                 debugText = "No connectors found!";
@@ -163,66 +173,113 @@ namespace GlacialisTechFacility
             //LCD.WritePublicText ( names, false );
         }
 
+        /// <summary>
+        /// Status for docking on the grid
+        /// </summary>
         private void Docking_Status ()
         {
             Fecth_Connectors ();
 
+            //  When using block group
             if ( G_useBlockGroup == true )
             {
-                for ( int j = 0; j < G_screens.Count; j++ )
-                {
-                    G_screens [j].WritePublicText ( "<---Ship Status--->\n", false );
-                }
+                Group_Docking ();
+            }
+            else    //  Using single lcd
+            {
+                Single_Docking ();
+            }
+        }
 
-                for ( int i = 0; i < connectors.Count; i++ )
+        /// <summary>
+        /// Using LCD blockgroup
+        /// </summary>
+        private void Group_Docking ()
+        {
+            for ( int j = 0; j < G_screens.Count; j++ )
+            {
+                G_screens [j].WritePublicText ( "<---Ship Status--->\n", false );
+            }
+
+            // Looping trough connectors
+            for ( int i = 0; i < connectors.Count; i++ )
+            {
+                //  If connectors are connected
+                if ( connectors [i].Status == MyShipConnectorStatus.Connected )
                 {
-                    if ( connectors [i].Status == MyShipConnectorStatus.Connected )
+                    //  Looping though screens and displaying info
+                    for ( int j = 0; j < G_screens.Count; j++ )
                     {
-                        if ( G_useBlockGroup == true )
+                        //  If a screen has a tag show advanced version
+                        if ( G_screens [i].CustomName.Contains ( LCD_TAGS [1] ) )
                         {
-                            for ( int j = 0; j < G_screens.Count; j++ )
-                            {
-                                G_screens [j].WritePublicText ( $"{connectors [i].CustomName}\n<>\n{connectors [i].OtherConnector.CubeGrid.CustomName}\n\n", true );
-                            }
+                            G_screens [j].WritePublicText ( $"{connectors [i].CustomName}\n<>\n{connectors [i].OtherConnector.CubeGrid.CustomName}\n\n", true );
+                        }
+                        else    //  If there's no tag show simple version
+                        {
+                            G_screens[i].WritePublicText ( $"Locked: {Get_Amount_Connected ()}/{connectors.Count}\n", true );
                         }
                     }
-                    else if ( connectors [i].Status == MyShipConnectorStatus.Connectable && G_useBlockGroup == true )
+                }
+                //  If connecters are connectable
+                else if ( connectors [i].Status == MyShipConnectorStatus.Connectable && G_useBlockGroup == true )
+                {
+                    //  Looping though screens and displaying info
+                    for ( int j = 0; j < G_screens.Count; j++ )
                     {
-                        for ( int j = 0; j < G_screens.Count; j++ )
+                        //  If a screen has a tag
+                        if ( G_screens [i].CustomName.Contains ( LCD_TAGS [1] ) )
                         {
                             G_screens [j].WritePublicText ( $"{connectors [i].CustomName}\n</>\n{connectors [i].OtherConnector.CubeGrid.CustomName}\n\n", true );
                         }
                     }
-                    else
+                }
+                //  If connectors are not connected
+                else
+                {
+                    //  Looping though screens and displaying info
+                    for ( int j = 0; j < G_screens.Count; j++ )
                     {
-                        if ( G_useBlockGroup == true )
+                        //  If a screen has a tag
+                        if ( G_screens [i].CustomName.Contains ( LCD_TAGS [1] ) )
                         {
-                            for ( int j = 0; j < G_screens.Count; j++ )
-                            {
-                                G_screens [j].WritePublicText ( $"{connectors [i].CustomName}\n()\n", true );
-                            }
+                            G_screens [j].WritePublicText ( $"{connectors [i].CustomName}\n()\n", true );
                         }
-
                     }
                 }
             }
-            else
-            {
-                if ( G_LCD != null )
-                {
-                    G_LCD.WritePublicText ( "<---Ship Status--->\n", false );
-                    int lockedCount = 0;
+        }
 
-                    for ( int i = 0; i < connectors.Count; i++ )
-                    {
-                        if ( connectors [i].Status == MyShipConnectorStatus.Connected )
-                        {
-                            lockedCount++;
-                        }
-                    }
-                    G_LCD.WritePublicText ( $"Connectors: {connectors.Count}\n", true );
-                    G_LCD.WritePublicText ( $"Locked: {lockedCount}/{connectors.Count}\n", true );
+        /// <summary>
+        /// Get the amount of connecters currently connected
+        /// </summary>
+        /// <returns></returns>
+        private int Get_Amount_Connected ()
+        {
+            int lockedCount = 0;
+
+            //  Looping trough all connectors
+            for ( int i = 0; i < connectors.Count; i++ )
+            {
+                //  If connectors is connected count them up
+                if ( connectors [i].Status == MyShipConnectorStatus.Connected )
+                {
+                    lockedCount++;
                 }
+            }
+
+            return lockedCount;
+        }
+
+        /// <summary>
+        /// Using single LCD mode
+        /// </summary>
+        private void Single_Docking ()
+        {
+            if ( G_LCD != null )
+            {
+                G_LCD.WritePublicText ( "<---Ship Status--->\n", false );
+                G_LCD.WritePublicText ( $"Locked: {Get_Amount_Connected ()}/{connectors.Count}\n", true );
             }
         }
 
